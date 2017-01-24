bd9999d974642e66cb9ec68eff8a01e696a99226
isabelrios <isabelrios@gmail.com>
2017-01-24 16:48:50 +0100

    fix package error and new check in test

--- Selenium_Tests/testSearchLocationWithSensor.js
+++ Selenium_Tests/testSearchLocationWithSensor.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var path = require('chromedriver').path;
+var chai = require("chai");
+chai.should();
 
 const By = require('selenium-webdriver').By;
 const until = webdriver.until;
@@ -65,6 +67,18 @@ describe('Show main page', function() {
                     .getText()
                     .then((text) => {
                         assert.equal(text, 'Sensor')
+                    });
+        });
+
+        it('should get sensor measurement', function(){
+            //Not the best solution, but need to wait till the view is loaded. Need to find a better solution
+            driver.sleep(1000)
+            this.timeout(mochaTimeOut);
+            return driver.wait(webdriver.until.elementIsVisible(driver.findElement(webdriver.By.xpath('//*[@id="root"]/div/div/div[1]/div[2]/div/div[2]/div[1]/div[4]/div/div/span'),5000)))
+            .getText()
+                    .then((value) => {
+                        chai.assert.isAbove(value, '0', 'Sensor value is equal or below 0')
+                        chai.assert.isBelow(parseInt(value), '100', 'Sensor value is higher than 100')
                     });
         });
     });
